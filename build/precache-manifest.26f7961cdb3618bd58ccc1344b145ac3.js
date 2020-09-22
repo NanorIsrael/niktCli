3cfbe365048a4c808b4f9fb5304b14a83afbed5a
israel nanor <israelnanor88@gmail.com>
2020-09-22 19:39:33 +0000

    deploytoGithub

---
+++ build/precache-manifest.26f7961cdb3618bd58ccc1344b145ac3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ab0ad2462f4adcddd2ab0aeecfb123",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e0cd7e74fcc3a036a4d",
+    "url": "/static/css/2.b0e1eb24.chunk.css"
+  },
+  {
+    "revision": "9cbee221cafabef41c4a",
+    "url": "/static/css/main.2907676b.chunk.css"
+  },
+  {
+    "revision": "7e0cd7e74fcc3a036a4d",
+    "url": "/static/js/2.21e02ff9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.21e02ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cbee221cafabef41c4a",
+    "url": "/static/js/main.0fdb02a0.chunk.js"
+  },
+  {
+    "revision": "12f5e0b116dc99461597",
+    "url": "/static/js/runtime-main.6c77d518.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
